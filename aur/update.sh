4bd49423a2502fcb56231016f5b6bc3347d55107
Xu FaSheng <fasheng.xu@gmail.com>
2014-06-25 00:15:36 +0800

    aur/deepin-ui: update

--- aur/update.sh
+++ aur/update.sh
@@ -98,7 +98,7 @@ download_debian_source
 # update_pkg "deepin-media-player"
 # update_pkg "deepin-music-player"
 # update_pkg "deepin-screenshot"
-update_pkg "deepin-terminal"
-# update_pkg "deepin-ui"
+# update_pkg "deepin-terminal"
+update_pkg "deepin-ui"
 
 # rm -vf "${tmp_source_file}"
