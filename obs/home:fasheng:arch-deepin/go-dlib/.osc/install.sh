f99d6da81bb41ee70e388ad460720b6770241501
xufasheng <fasheng.xu@gmail.com>
2014-08-26 16:16:12 +0800

    osc: remove home:fasheng:arch-deepin

--- obs/home:fasheng:arch-deepin/go-dlib/.osc/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# don't direct use this
-PWD1=`pwd`
-mkdir src && cd src && ln -sv ../dlib .
-echo ${PWD1}
-GOPATH=${PWD1} go install dlib/pseudo
-GOPATH=${PWD1} go install dlib/dbus/proxyer
-GOLIB=`go env GOHOSTOS`_`go env GOHOSTARCH`
-#rm ${PWD1}/pkg/${GOLIB}/dlib/pseudo.a
