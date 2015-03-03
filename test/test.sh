8954f14d3642693489a4210e7fc5b181645c2d3a
xufasheng <fasheng.xu@gmail.com>
2015-03-03 11:28:11 +0800

    update.sh: refactor code, provide solution for aur, obs and third-party repos

---
+++ test/test.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+cd ..
+source ./libpkginfo.sh
+cd -
+
+# override variables
+mirror="http://packages.linuxdeepin.com"
+fileurl_prefix="deepin"
+local_sources=(
+    "./reposources/main"
+    "./reposources/non-free"
+    "./reposources/universe"
+)
+
+# check
+check() {
+    local value="${1}"
+    local want="${2}"
+    if [ x"${value}" != x"${want}" ]; then
+        error "value=${value}, want=${want}"
+    fi
+}
+
+# tests
+test_get_pkginfo() {
+    get_pkginfo "dde-daemon"
+    check "${pkg_name}" "dde-daemon"
+    check "${pkg_version}" "0.0.1+20150209101554~660a820a61"
+    check "${pkg_version_fixed}" "0.0.1.20150209101554.660a820a61"
+    check "${pkg_directory}" "pool/main/d/dde-daemon"
+    check "${pkg_file}" "dde-daemon_0.0.1+20150209101554~660a820a61.tar.gz"
+    check "${pkg_fileurl}" "http://packages.linuxdeepin.com/deepin/pool/main/d/dde-daemon/dde-daemon_0.0.1+20150209101554~660a820a61.tar.gz"
+    check "${pkg_sha256sum}" "db271a63dc972c41a29bbbb6a30af897c4f7efe28e8ad3dbac37e061c508dd2c"
+    check "${obs_host}" "packages.linuxdeepin.com"
+    check "${obs_protocol}" "http"
+    check "${obs_path}" "/deepin/pool/main/d/dde-daemon/dde-daemon_0.0.1+20150209101554~660a820a61.tar.gz"
+}
+
+test_get_pkginfo_multiple_sources() {
+    get_pkginfo "vte"
+    check "${pkg_name}" "vte"
+    check "${pkg_version}" "0.28.2-6deepin7~saucy"
+    check "${pkg_version_fixed}" "0.28.2"
+    check "${pkg_directory}" "pool/main/v/vte"
+    check "${#pkg_files[@]}" 3
+    check "${pkg_files[0]}" "vte_0.28.2-6deepin7~saucy.dsc"
+    check "${pkg_files[1]}" "vte_0.28.2.orig.tar.xz"
+    check "${pkg_files[2]}" "vte_0.28.2-6deepin7~saucy.debian.tar.gz"
+    check "${#pkg_fileurls[@]}" 3
+    check "${pkg_fileurls[0]}" "http://packages.linuxdeepin.com/deepin/pool/main/v/vte/vte_0.28.2-6deepin7~saucy.dsc"
+    check "${pkg_fileurls[1]}" "http://packages.linuxdeepin.com/deepin/pool/main/v/vte/vte_0.28.2.orig.tar.xz"
+    check "${pkg_fileurls[2]}" "http://packages.linuxdeepin.com/deepin/pool/main/v/vte/vte_0.28.2-6deepin7~saucy.debian.tar.gz"
+    check "${#pkg_sha256sums[@]}" 3
+    check "${pkg_sha256sums[0]}" "44f390fea99f6ea13ddda98ed16bf20f9d4b2935e6e81d305b608c4485361a58"
+    check "${pkg_sha256sums[1]}" "ee52b91ecab31d0a64399896ce0c3515e579ea8ac212a00eb9b0895c58f001fe"
+    check "${pkg_sha256sums[2]}" "b63a344541f0feebddac0232a8d7196d22d4819ec32cbd6cb39e33b2ba50e940"
+    check "${#obs_paths[@]}" 3
+    check "${obs_paths[0]}" "/deepin/pool/main/v/vte/vte_0.28.2-6deepin7~saucy.dsc"
+    check "${obs_paths[1]}" "/deepin/pool/main/v/vte/vte_0.28.2.orig.tar.xz"
+    check "${obs_paths[2]}" "/deepin/pool/main/v/vte/vte_0.28.2-6deepin7~saucy.debian.tar.gz"
+}
+
+test_get_pkginfo_fixed_version() {
+    get_pkginfo "dde-account-faces"
+    check "${pkg_version}" "1.0.7-1~trusty"
+    check "${pkg_version_fixed}" "1.0.7.1"
+
+    get_pkginfo "deepin-artwork"
+    check "${pkg_version}" "2014-4"
+    check "${pkg_version_fixed}" "2014.4"
+
+    get_pkginfo "deepin-artwork-themes"
+    check "${pkg_version}" "14.04.3+20141204110221~5bf78d7f42"
+    check "${pkg_version_fixed}" "14.04.3.20141204110221.5bf78d7f42"
+
+    get_pkginfo "deepin-bug-reporter"
+    check "${pkg_version}" "1+20141029130135~d2afb454d7"
+    check "${pkg_version_fixed}" "1.20141029130135.d2afb454d7"
+
+    get_pkginfo "deepin-cursor-theme"
+    check "${pkg_version}" "2014~4-2"
+    check "${pkg_version_fixed}" "2014.4.2"
+
+    get_pkginfo "deepin-default-settings"
+    check "${pkg_version}" "2014~12-35~trusty"
+    check "${pkg_version_fixed}" "2014.12.35"
+
+    get_pkginfo "deepin-icon-theme-v2013"
+    check "${pkg_version}" "1.0-1"
+    check "${pkg_version_fixed}" "1.0.1"
+
+    get_pkginfo "vte"
+    check "${pkg_version}" "0.28.2-6deepin7~saucy"
+    check "${pkg_version_fixed}" "0.28.2"
+}
+
+# simple test runner
+appfile="${0}"
+get_all_tests() {
+    grep -o '^test_[^(]*' "${appfile}"
+}
+run_all_tests() {
+    for t in $(get_all_tests); do
+        msg "run $t"
+        $t
+    done
+}
+run_all_tests
--- update.sh
+++ update.sh
@@ -1,68 +1,237 @@
 #!/bin/bash
 
-aur_dir="aur"
-obs_dir="obs/home:metakcahura:arch-deepin"
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
 
-obs_pkgs_dev=(
-    go-dlib
-    dbus-generator
-    dbus-factory
-)
-obs_pkgs_extra=(
-     deepin-music-player
-     deepin-movie
-     deepin-screenshot
-     deepin-terminal
-     deepin-game-center
-)
-obs_pkgs=(
-    dde-api
-    dde-daemon
-    dde-workspace
-    startdde
-    deepin-qml-widgets
-    qml-dde-dock-applet
-    dde-control-center
-    deepin-menu
-    deepin-notifications
-    deepin-gsettings
-    deepin-default-gsettings
-    dde-account-faces
-    deepin-artwork
-    deepin-artwork-themes
-    deepin-cursor-theme
-    deepin-compiz
-    libdeepin-webkit
-    deepin-icon-theme
-)
+# depends: curl, jq, makepkg, mkaurball, burp
 
-update_aur() {
-    (cd "${aur_dir}"; ./update.sh)
+source ./libhelper.sh
+source ./libpkginfo.sh
+source ./libjson.sh
+
+###* Update package
+update_pkg() {
+  local pkgname="${1}"
+  if do_update_pkg "${pkgname}"; then
+      log_success "result_update.log" "${pkgname}"
+  else
+    log_failed "result_update.log" "${pkgname}"
+  fi
+  # copy files to release dir
+  mkdir -p "${pkgbuilddir}/${pkgname}"
+  find "${templatedir}/${pkgname}" -maxdepth 1 -type f -not -name '*.in' -exec cp -vf '{}' "${obsdir}/${pkgname}/" ';'
+  find "${templatedir}/${pkgname}" -maxdepth 1 -type f -not '(' -name '*.in' -or -name '_service' ')' -exec cp -vf '{}' "${pkgbuilddir}/${pkgname}/" ';'
+}
+do_update_pkg() {
+  local pkgname="${1}"
+  msg "update package: ${pkgname}"
+  case "${pkgname}" in
+    "deepin-vte") gen_template_multi_sources "${pkgname}" 1 2 || return 1;;
+    *) gen_template "${pkgname}" || return 1;;
+  esac
+  update_package_state "${pkgname}" "${pkg_version}"
 }
 
-update_obs_dev() {
-    for p in ${obs_pkgs_dev[@]}; do
-        do_obs_trigger_rerun "${obs_dir}/${p}"
+gen_template() {
+  local pkgname="${1}"
+  get_pkginfo "$(get_package_originrepodir ${pkgname})"
+  msg2 "pkg_version: ${pkg_version}"
+  msg2 "pkg_version_fixed: ${pkg_version_fixed}"
+  msg2 "pkg_directory: ${pkg_directory}"
+  msg2 "pkg_file: ${pkg_file}"
+  msg2 "pkg_fileurl: ${pkg_fileurl}"
+  msg2 "pkg_sha256sum: ${pkg_sha256sum}"
+  (
+    cd "${templatedir}/${pkgname}"
+    sed -e "s=@PKGVER@=${pkg_version}=g" \
+        -e "s=@PKGVER_FIXED@=${pkg_version_fixed}=g" \
+        -e "s=@SOURCE@=${pkg_fileurl}=g" \
+        -e "s=@SHA256SUM@=${pkg_sha256sum}=g" "${pkgbuild_in}" > PKGBUILD
+    if [ -f "${service_in}" ]; then
+        sed -e "s=@HOST@=${obs_host}=g" \
+            -e "s=@PROTOCOL@=${obs_protocol}=g" \
+            -e "s=@PATH@=${obs_path}=g" "${service_in}" > _service
+    fi
+  )
+}
+
+gen_template_multi_sources() {
+  local pkgname="${1}"; shift
+  local fileindexes=("${@}")
+  get_pkginfo "$(get_package_originrepodir ${pkgname})"
+  msg2 "pkg_version: ${pkg_version}"
+  msg2 "pkg_version_fixed: ${pkg_version_fixed}"
+  msg2 "pkg_directory: ${pkg_directory}"
+  for i in "${fileindexes[@]}"; do
+    msg2 "pkg_files[${i}]: ${pkg_files[${i}]}"
+    msg2 "pkg_fileurls[${i}]: ${pkg_fileurls[${i}]}"
+    msg2 "pkg_sha256sums[${i}]: ${pkg_sha256sums[${i}]}"
+  done
+  (
+    cd "${templatedir}/${pkgname}"
+    sed -e "s=@PKGVER@=${pkg_version}=g" \
+        -e "s=@PKGVER_FIXED@=${pkg_version_fixed}=g" "${pkgbuild_in}" > PKGBUILD
+    if [ -f "${service_in}" ]; then
+        sed -e "s=@HOST@=${obs_host}=g" \
+            -e "s=@PROTOCOL@=${obs_protocol}=g" "${service_in}" > _service
+    fi
+    local tagi=1
+    for i in "${fileindexes[@]}"; do
+      sed -i -e "s=@SOURCE${tagi}@=${pkg_fileurls[${i}]}=g" \
+          -e "s=@SHA256SUM${tagi}@=${pkg_sha256sums[${i}]}=g" PKGBUILD
+      if [ -f "${service_in}" ]; then
+          sed -i -e "s=@PATH${tagi}@=${obs_paths[${i}]}=g" _service
+      fi
+      ((tagi++))
     done
-}
-update_obs_extra() {
-    for p in ${obs_pkgs_extra[@]}; do
-        do_obs_trigger_rerun "${obs_dir}/${p}"
-    done
-}
-update_obs() {
-    for p in ${obs_pkgs_dev[@]} ${obs_pkgs_extra[@]} ${obs_pkgs[@]}; do
-        do_obs_trigger_rerun "${obs_dir}/${p}"
-    done
-}
-do_obs_trigger_rerun() {
-    prj_dir="${1}"
-    prj_name="$(basename ${1})"
-    (
-        echo "==> trigger re-run for ${prj_name}"
-        cd "${obs_dir}/${p}"
-        osc service rr
-    )
+  )
 }
 
-update_"$1"
+###* Run makepkg
+run_makepkg() {
+  local pkgname="${1}"
+  if do_run_makepkg "${pkgname}"; then
+      log_success "result_makepkg.log" "${pkgname}"
+  else
+    log_failed "result_makepkg.log" "${pkgname}"
+  fi
+}
+do_run_makepkg() {
+  local pkgname="${1}"
+  (cd "${pkgbuilddir}/${pkgname}"; rm -rf src pkg; makepkg -f) || return 1
+}
+
+###* Upload to AUR
+aur_upload() {
+  local pkgname="${1}"
+  if do_aur_upload "${pkgname}"; then
+      log_success "result_aur_upload.log" "${pkgname}"
+  else
+    log_failed "result_aur_upload.log" "${pkgname}"
+  fi
+}
+do_aur_upload() {
+  local pkgname="${1}"
+
+  # generate a new package source
+  (cd "${pkgbuilddir}/${pkgname}"; mkaurball -f) || return 1
+
+  local pkgsrc="$(ls --format=single-column --sort=time "${pkgname}"/*.src.tar.gz | head -1)"
+  local upcmd="$(printf "${aur_upload_cmd}" "${pkgsrc}")"
+  printf "Uploading package source to AUR: %s...\n" "${pkgsrc}"
+  printf "Upload command: %s\n" "${upcmd}"
+  eval "${upcmd}" || return 1
+}
+
+###* Main
+# basic configuration
+set -e
+LC_ALL=C
+appfile="${0}"
+appname="$(basename $0)"
+
+# global variables
+templatedir="template"
+pkgbuilddir="pkgbuild"
+aurdir="aur"
+obsdir="obs/home:metakcahura:arch-deepin"
+pkgbuild_in="PKGBUILD.in"
+service_in="_service.in"
+aur_upload_cmd="burp -v %s"
+
+show_usage() {
+  cat <<EOF
+${appname} [options]
+options:
+    -p, --package
+        only update target package
+    --force-updated
+        mark all package updated
+    --ignore-reposources
+        do not download repo sources this time
+    --makepkg
+        run makepkg for updated packages
+    --aur-upload
+        upload updated packages to aur
+    -h, --help
+        show this message
+EOF
+  exit 1
+}
+
+# arguments
+arg_show_usage=
+arg_package=
+arg_force_updated=
+arg_ignore_reposources=
+arg_makepkg=
+arg_aur_upload=
+
+while [ $# -gt 0 ]; do
+  case "${1}" in
+    -h|--help) arg_show_usage=1; break;;
+    -p|--package) arg_package="${2}"; shift; shift;;
+    --force-updated) arg_force_updated=1; shift;;
+    --ignore-reposources) arg_ignore_reposources=1; shift;;
+    --makepkg) arg_makepkg=1; shift;;
+    --aur-upload) arg_aur_upload=1; shift;;
+    *) shift;;
+  esac
+done
+
+if [ "${arg_show_usage}" ]; then
+    show_usage
+fi
+
+if [ ! "${arg_ignore_reposources}" ]; then
+    download_repo_sources
+fi
+
+pkgs=()
+if [ "${arg_package}" ]; then
+    pkgs=("${arg_package}")
+else
+  pkgs=($(get_all_packages))
+fi
+
+for p in "${pkgs[@]}"; do
+  if [ "${arg_force_updated}" ]; then
+      set_package_updated "${p}" true
+  fi
+
+  if [ "${arg_makepkg}" ]; then
+      if get_package_updated "${p}"; then
+          run_makepkg "${p}"
+      fi
+  elif [ "${arg_aur_upload}" ]; then
+      if get_package_updated "${p}"; then
+          aur_upload "${p}"
+      fi
+  else
+    update_pkg "${p}"
+  fi
+
+  # ensure package updated again for that it may be changed when
+  # updating package state
+  if [ "${arg_force_updated}" ]; then
+      set_package_updated "${p}" true
+  fi
+done
+
+# Local Variables:
+# mode: sh
+# mode: orgstruct
+# orgstruct-heading-prefix-regexp: "^\s*###"
+# End:
