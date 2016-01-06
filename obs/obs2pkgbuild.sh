c5e07a1d265dec905c7958933600bbde2ccd09ac
Xu Fasheng <fasheng.xu@gmail.com>
2016-01-06 11:37:57 +0800

    remove obs, test

--- obs/obs2pkgbuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -vf home:metakcahura:arch-deepin/qtav/PKGBUILD ../pkgbuild/qtav/
-cp -vf home:metakcahura:arch-deepin/python2-xpyb/{PKGBUILD,64.patch} ../pkgbuild/python2-xpyb/
-cp -vf home:metakcahura:arch-deepin/python2-xpybutil/PKGBUILD ../pkgbuild/python2-xpybutil/
